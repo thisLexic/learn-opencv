eb674f775201983c689ae8d706fefa6dc648b9bd
thislexic <benvillabroza1999@gmail.com>
2021-08-05 15:28:08 +0800

    added ben face

--- face_detection/3_face_img_print.py
+++ face_detection/3_face_img_print.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('../haarcascades/haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
 
